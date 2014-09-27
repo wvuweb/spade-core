23c3c413b0d38e6191f5d8b7fd3ab653f673899a
Dave Olsen <dmolsen@gmail.com>
2014-09-26 21:16:57 -0400

    properly setting up vars for the database user/pass check

--- src/Spade/Model.php
+++ src/Spade/Model.php
@@ -23,11 +23,17 @@ class Model {
 	*/
 	public static function init() {
 		
+		// get various user info
+		$defaultUser   = Config::getOption("user");
+		$defaultPass   = Config::getOption("pass");
+		$readwriteUser = Config::getOption("readwrite.user");
+		$readwritePass = Config::getOption("readwrite.user");
+		
 		// set-up the database connection info
 		$name = Config::getOption("name");
 		$host = Config::getOption("host");
-		$user = (empty(Config::getOption("readwrite.user")) ? Config::getOption("user") : Config::getOption("readwrite.user");
-		$pass = (empty(Config::getOption("readwrite.pass")) ? Config::getOption("pass") : Config::getOption("readwrite.pass");
+		$user = (!$readwriteUser || empty($readwriteUser)) ? $defaultUser : $readwriteUser;
+		$pass = (!$readwritePass || empty($readwritePass)) ? $defaultPass : $readwritePass;
 		$port = Config::getOption("port");
 		Pheasant::setup("mysql://".$user.":".$pass."@".$host.":".$port."/".$name);
 		
