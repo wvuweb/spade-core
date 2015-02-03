3f318d4ded9f7d37af096b69a44672765a797e5f
Dave Olsen <dmolsen@gmail.com>
2015-02-03 09:55:21 -0500

    test commit for brett

--- src/Spade/App.php
+++ src/Spade/App.php
@@ -6,7 +6,7 @@
  * Copyright (c) 2014 Dave Olsen
  *
  * Reads in a YAML configuration file and makes the attributes available to the project 
- * Adding more content since that was just a whitespace commit
+ * Adding more content since that was just a whitespace commit. Some more of a commit.
  *
  */
 
