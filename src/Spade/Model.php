54b7db5c9108e37e83b3e62dbe22624bfcaf6e96
Steven Spriggs <steven.spriggs@gmail.com>
2015-02-04 15:26:24 -0500

    convert \Spade\Model to use \Illuminate Classes for database connection

--- src/Spade/Model.php
+++ src/Spade/Model.php
@@ -14,28 +14,45 @@ namespace Spade;
 use \Pheasant;
 use \Spade\Config;
 use \Spade\Timer;
+use \Illuminate\Database\Capsule\Manager as Capsule;
+use \Illuminate\Events\Dispatcher;
+use \Illuminate\Container\Container;
 
 class Model {
-	
+
 	/**
 	* Set-up the database connection
 	*/
 	public static function init() {
-		
-		// get various user info
-		$defaultUser   = Config::getOption("user");
-		$defaultPass   = Config::getOption("pass");
-		$readwriteUser = Config::getOption("readwrite.user");
-		$readwritePass = Config::getOption("readwrite.pass");
-		
-		// set-up the database connection info
-		$name = Config::getOption("name");
-		$host = Config::getOption("host");
-		$user = (!$readwriteUser || empty($readwriteUser)) ? $defaultUser : $readwriteUser;
-		$pass = (!$readwritePass || empty($readwritePass)) ? $defaultPass : $readwritePass;
-		$port = Config::getOption("port");
-		Pheasant::setup("mysql://".$user.":".$pass."@".$host.":".$port."/".$name);
-		
-	}
-	
+	// Database connection
+	$capsule = new Capsule;
+
+	$defaultUser   = Config::getOption("user");
+	$defaultPass   = Config::getOption("pass");
+	$readwriteUser = Config::getOption("readwrite.user");
+	$readwritePass = Config::getOption("readwrite.pass");
+
+	$user = (!$readwriteUser || empty($readwriteUser)) ? $defaultUser : $readwriteUser;
+	$pass = (!$readwritePass || empty($readwritePass)) ? $defaultPass : $readwritePass;
+
+	$capsule->addConnection([
+			'driver'    => 'mysql',
+			'host'      => Config::getOption("host"),
+			'database'  => Config::getOption("name"),
+			'username'  => $user,
+			'password'  => $pass,
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+	]);
+
+	$capsule->setEventDispatcher(new Dispatcher(new Container));
+
+	// Make this Capsule instance available globally via static methods... (optional)
+	$capsule->setAsGlobal();
+
+	// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+	$capsule->bootEloquent();
+}
+
 }
