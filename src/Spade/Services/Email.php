52f819ce01e44ca8f5f73425496e83aaf656a427
Dave Olsen <dmolsen@gmail.com>
2015-02-04 13:57:33 -0500

    simplifying the set-up and delivery for emails

--- src/Spade/Services/Email.php
+++ src/Spade/Services/Email.php
@@ -19,41 +19,58 @@ class Email {
 	
 	/**
 	* Send the email
-	* @param  {String}       the name of the email so attributes can be pulled from the config
-	* @param  {String}       the subject for the email
-	* @param  {String}       the body for the email
+	* @param  {Array}        various options that can be used for the email
 	*
 	* @param  {Object}       the status of the sent email
 	*/
-	public static function send($emailName,$subject,$body) {
+	public static function send($emailName = "", $options = array()) {
 		
-		// populate default vars
-		$optionBase = self::getOptionBase($emailName);
-		$to         = Config::getOption($optionBase."to");
-		$cc         = Config::getOption($optionBase."cc");
-		$from       = Config::getOption($optionBase."from");
-		
-		// make sure certain vars are arrays for swiftmailer
-		$to         = (!is_array($to))   ? array($to)   : $to;
-		$from       = (!is_array($from)) ? array($from) : $from;
-		
-		// swiftmailer connection info
-		$transport  = \Swift_SmtpTransport::newInstance('smtp.wvu.edu', 25);
-		$mailer     = \Swift_Mailer::newInstance($transport);
-		
-		// set-up the message
-		$message    = \Swift_Message::newInstance();
-		$message->setTo($to);
-		$message->setFrom($from);
-		$message->setSubject($subject);
-		$message->setBody($body);
-		if ($cc) {
-			$cc = (!is_array($cc)) ? array($cc) : $cc;
-			$message->setCc($cc);
+		// if email isn't enabled just silently fail as if it were a success
+		if (!Config::getOption("enableEmail")) {
+			return true;
 		}
 		
-		// send it
-		$result = $mailer->send($message);
+		// make sure we have an email to pull options for
+		if (empty($emailName)) {
+			Render::warning("need to pass an emailName");
+			return false;
+		}
+		
+		// populate default vars
+		$optionBase   = self::getOptionBase($options["emailName"]);
+		$data         = isset($options["data"])         ? $options["data"] : array();
+		$to           = isset($options["to"])           ? $options["to"] : Config::getOption($optionBase."to");
+		$cc           = isset($options["cc"])           ? $options["cc"] : Config::getOption($optionBase."cc");
+		$from         = isset($options["from"])         ? $options["from"] : Config::getOption($optionBase."from");
+		$subject      = isset($options["subject"])      ? $options["subject"] : Config::getOption($optionBase."subject");
+		$templatePath = isset($options["templatePath"]) ? $options["templatePath"] : Config::getOption($optionBase."template");
+		
+		// render the email body
+		$body         = Render::template($templatePath,$data);
+		
+		// make sure certain vars are arrays for swiftmailer
+		$to           = (!is_array($to))   ? array($to)   : $to;
+		$from         = (!is_array($from)) ? array($from) : $from;
+		
+		// swiftmailer connection info
+		$transport    = \Swift_SmtpTransport::newInstance('smtp.wvu.edu', 25);
+		$mailer       = \Swift_Mailer::newInstance($transport);
+		
+		// set-up and send the message
+		try {
+			$message      = \Swift_Message::newInstance();
+			$message->setTo($to);
+			$message->setFrom($from);
+			$message->setSubject($subject);
+			$message->setBody($body);
+			if ($cc) {
+				$cc = (!is_array($cc)) ? array($cc) : $cc;
+				$message->setCc($cc);
+			}
+			$result = $mailer->send($message);
+		} catch (\Exception $e) {
+			return false;
+		}
 		
 		return $result;
 		
