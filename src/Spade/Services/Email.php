1d4a1f5c282063a319ea8b2f50243a4f8859a0d3
Steven Spriggs <steven.spriggs@gmail.com>
2015-03-27 14:32:53 -0400

    add html ability to send formated HTML emails

--- src/Spade/Services/Email.php
+++ src/Spade/Services/Email.php
@@ -16,7 +16,7 @@ use \Spade\Render;
 use \Spade\Timer;
 
 class Email {
-	
+
 	/**
 	* Send the email
 	* @param  {Array}        various options that can be used for the email
@@ -24,38 +24,40 @@ class Email {
 	* @param  {Object}       the status of the sent email
 	*/
 	public static function send($emailName = "", $options = array()) {
-		
+
 		// if email isn't enabled just silently fail as if it were a success
 		if (!Config::getOption("enableEmail")) {
 			return true;
 		}
-		
+
 		// make sure we have an email to pull options for
 		if (empty($emailName)) {
 			Render::warning("need to pass an emailName");
 			return false;
 		}
-		
+
 		// populate default vars
-		$optionBase   = self::getOptionBase($emailName);
-		$data         = isset($options["data"])         ? $options["data"] : array();
-		$to           = isset($options["to"])           ? $options["to"] : Config::getOption($optionBase."to");
-		$cc           = isset($options["cc"])           ? $options["cc"] : Config::getOption($optionBase."cc");
-		$from         = isset($options["from"])         ? $options["from"] : Config::getOption($optionBase."from");
-		$subject      = isset($options["subject"])      ? $options["subject"] : Config::getOption($optionBase."subject");
-		$templatePath = isset($options["templatePath"]) ? $options["templatePath"] : Config::getOption($optionBase."template");
-		
+		$optionBase   			= self::getOptionBase($emailName);
+		$data         			= isset($options["data"])         		? $options["data"] : array();
+		$to           			= isset($options["to"])           		? $options["to"] : Config::getOption($optionBase."to");
+		$cc           			= isset($options["cc"])           		? $options["cc"] : Config::getOption($optionBase."cc");
+		$from         			= isset($options["from"])         		? $options["from"] : Config::getOption($optionBase."from");
+		$subject      			= isset($options["subject"])      		? $options["subject"] : Config::getOption($optionBase."subject");
+		$templatePath 			= isset($options["templatePath"]) 		? $options["templatePath"] : Config::getOption($optionBase."template");
+		$htmlTemplatePath		= isset($options["htmlTemplatePath"]) ? $options["htmlTemplatePath"] : Config::getOption($optionBase."html_template");
+
 		// render the email body
 		$body         = Render::template($templatePath,$data,false);
-		
+		$body_html 	= $htmlTemplatePath ? Render::template($htmlTemplatePath,$data,false) : false;
+
 		// make sure certain vars are arrays for swiftmailer
 		$to           = (!is_array($to))   ? array($to)   : $to;
 		$from         = (!is_array($from)) ? array($from) : $from;
-		
+
 		// swiftmailer connection info
 		$transport    = \Swift_SmtpTransport::newInstance('smtp.wvu.edu', 25);
 		$mailer       = \Swift_Mailer::newInstance($transport);
-		
+
 		// set-up and send the message
 		try {
 			$message      = \Swift_Message::newInstance();
@@ -63,6 +65,16 @@ class Email {
 			$message->setFrom($from);
 			$message->setSubject($subject);
 			$message->setBody($body);
+
+			if ($body_html){
+				// set content-type and charset
+				$type = $message->getHeaders()->get('Content-Type');
+				$type->setValue('text/html');
+				$type->setParameter('charset', 'utf-8');
+				// add HTML
+				$message->addPart($body_html,"text/html");
+			}
+
 			if ($cc) {
 				$cc = (!is_array($cc)) ? array($cc) : $cc;
 				$message->setCc($cc);
@@ -71,11 +83,11 @@ class Email {
 		} catch (\Exception $e) {
 			return false;
 		}
-		
+
 		return $result;
-		
+
 	}
-	
+
 	/**
 	* Get the option base string so we can properly set-up config option gets
 	* @param  {String}       the name of the email so attributes can be pulled from the config
@@ -83,11 +95,11 @@ class Email {
 	* @return {String}       the option base
 	*/
 	public static function getOptionBase($emailName) {
-		
+
 		$emailConfigBase = "emails";
 		$emailType       = Config::getOption("enableEmailDebug") ? "debug" : "default";
 		return $emailConfigBase.".".$emailName.".".$emailType.".";
-		
+
 	}
-	
+
 }
